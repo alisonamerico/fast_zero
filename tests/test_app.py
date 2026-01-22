48b4e50f8f97b867e63882f3ac95278ef74f624c
Alison Americo <alisonlira@proton.me>
2026-01-22 15:07:55 -0300

    add hello world in html

--- tests/test_app.py
+++ tests/test_app.py
@@ -11,3 +11,20 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_root_deve_retornar_html():
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert (
+        response.text
+        == """
+        <html>
+          <head>
+            <title> Nosso olá mundo!</title>
+          </head>
+          <body>
+            <h1> Olá Mundo </h1>
+          </body>
+        </html>"""
+    )
